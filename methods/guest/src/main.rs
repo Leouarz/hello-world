d85ccf9bf8291da056904403572ea709a22c18f0
Leouarz <ghalielouarzazi@hotmail.com>
2024-05-27 11:12:56 +0200

    add tokio

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -4,15 +4,28 @@
 
 
 use risc0_zkvm::guest::env;
+use tokio::runtime::Builder;
 
 risc0_zkvm::guest::entry!(main);
 
+async fn perform_async_task() {
+    6u32;
+}
 
 fn main() {
     // TODO: Implement your guest code here
 
     // read the input
     let input: u32 = env::read();
+    let rt = Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    
+    rt.block_on(async {
+        perform_async_task().await;
+    });
+
 
     // TODO: do something with the input
 
